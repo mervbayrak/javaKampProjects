ca87a06569eef359dfab3c5a132039087bfdaba2
MERVE BAYRAK <mervbayrak@hotmail.com>
2021-05-29 13:52:25 +0300

    First Commit

---
+++ northwind/src/main/java/kodlamaio/northwind/api/ProductsController.java
@@ -0,0 +1,36 @@
+package kodlamaio.northwind.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+	
+	private ProductService productService;
+	
+	@Autowired
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+//http://localhost:8080/api/products/getall
+	@GetMapping("/getall")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(Product product){
+		return new Result(true, "Ürü eklendi");
+	}
+}
